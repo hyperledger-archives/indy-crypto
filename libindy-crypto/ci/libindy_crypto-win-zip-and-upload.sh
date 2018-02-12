474b417f9f874fb80d9fcdde3d3d7c430b44198b
artem.ivanov <artem.ivanov@dsr-company.com>
2018-02-12 09:05:07 +0300

    Win Publishing problem
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- libindy-crypto/ci/libindy_crypto-win-zip-and-upload.sh
+++ libindy-crypto/ci/libindy_crypto-win-zip-and-upload.sh
@@ -18,12 +18,11 @@ number="$4"
 mkdir libindy_crypto-zip
 mkdir libindy_crypto-zip/lib
 cp -r ./include ./libindy_crypto-zip
-cp ./target/debug/*.dll ./libindy_crypto-zip/lib/
+cp ./target/release/*.dll ./libindy_crypto-zip/lib/
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('libindy_crypto-zip', 'libindy_crypto_$version.zip'); }"
 rm -rf ./libindy-zip
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.115
-mkdir /var/repository/repos/windows/libindy_crypto/$type
 mkdir /var/repository/repos/windows/libindy_crypto/$type/$version-$number
 cd /var/repository/repos/windows/libindy_crypto/$type/$version-$number
 put -r libindy_crypto_"$version".zip
